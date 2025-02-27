3dacd9b97e61ce047473df11f269122fe77f0f70
tomimoure8 <tomimoure8@gmail.com>
2025-02-27 18:15:54 -0300

    feat: Integrar Handlebars para vistas estaticas // Configura Handlebars y añade una vista estatica shop.handlebars para listar productos - Agrega express-handlebars al package.json - Actualiza server.js para renderizar la vista en la raiz

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
 const db = require('./src/config/db');
 const productsRouter = require('./routes/products');
 const cartsRouter = require('./routes/carts');
@@ -8,12 +10,17 @@ const PORT = 8080;
 const API_PRODUCTS = '/api/products';
 const API_CARTS = '/api/carts';
 
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'src/views'));
+
 app.use(express.json());
 app.use(API_PRODUCTS, productsRouter);
 app.use(API_CARTS, cartsRouter);
 
-app.get('/', (req, res) => {
-  res.send('Servidor de e-commerce en funcionamiento');
+app.get('/', async (req, res) => {
+  const products = await db.read('products');
+  res.render('shop', { products });
 });
 
 app.use((err, req, res, next) => {
